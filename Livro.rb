e45da916fbd1f4b1b26a8689f32cb2e079f728fd
Severino Silva <severinosilva.bc@gmail.com>
2017-10-09 18:29:16 -0300

    Implentando metodo para devolver os dados do livro

---
+++ Livro.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+class Livro
+	def initialize(autor, isbn="1", numero_de_paginas)
+		@autor = autor
+		@isbn = isbn
+		@numero_de_paginas = numero_de_paginas
+	end	
+
+	def to_s
+		"Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}"
+	end
+
+end
+
